417ea2462fc33160e3fb655beb7e4e5c8a738fbf
guardianprime <okongordian@gmail.com>
2025-07-25 21:08:09 +0100

    chore: added seeders and exercise catalog

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
+// db.js
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const CONNECTION_URL = process.env.MONGO_URI;
 
 function connectToMongoDB() {
-  mongoose.connect(CONNECTION_URL);
-
-  mongoose.connection.on("connected", () => {
-    console.log("successfuly connected to mongodb");
+  mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
 
-  mongoose.connection.on("error", () => {
-    console.log("failed to connect to mongodb");
+  mongoose.connection.on("connected", () => {
+    console.log("✅ Successfully connected to MongoDB");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.error("❌ MongoDB connection error:", err);
   });
 }
 
